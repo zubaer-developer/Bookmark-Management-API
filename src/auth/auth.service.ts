0b386d1341e31d700e8021f85d55056b55f506d0
ZUBAER <zubaer.developer@gmail.com>
2025-07-05 12:38:41 +0600

    User registration API created with validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
+import { AuthDto } from './dto/auth.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Hash } from 'crypto';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return { message: 'User signed up successfully' };
-  }
+  constructor(private prisma: PrismaService) {}
 
-  signin() {
-    return { message: 'User signed in successfully' };
-  }
+  async signup(userData: AuthDto) {
+    const hashedPassword = await argon.hash(userData.password);
+    try {
+      const newUser = await this.prisma.user.create({
+        data: {
+          email: userData.email,
+          hash: hashedPassword,
+        },
+      });
 
-  async checkUserPasswordHashing() {
-    const userPassword = '123456';
-    const hashedUserPassword = await argon.hash(userPassword);
-    console.log('User Original Password:', userPassword);
-    console.log('User Hashed Password:', hashedUserPassword);
-    return 'User password hashing is working for bookmark API!';
+      delete (newUser as any).hash;
+
+      return {
+        message: 'User created successfully',
+        user: newUser,
+      };
+    } catch (error) {
+      throw new ForbiddenException(
+        'Email already exists. Try with different email.',
+      );
+    }
   }
 }
