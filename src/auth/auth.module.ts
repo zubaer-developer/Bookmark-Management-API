ee5a3005f252c96b4351b11788100db45cab4eb4
ZUBAER <zubaer.developer@gmail.com>
2025-07-06 16:49:15 +0600

    Complete Authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'super-secret',
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   );
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Application is running on: ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
